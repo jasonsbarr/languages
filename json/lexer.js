7e5d576e42e0a05585fc6f06beb0dbdec4f117e3
Jason Barr <jason@jasonsbarr.com>
2021-04-28 08:14:51 -0500

    Add helper functions

---
+++ json/lexer.js
@@ -0,0 +1,23 @@
+function isDigit(char) {
+  return /[0-9]/.test(char);
+}
+
+function isPositiveDigit(char) {
+  return /[1-9]/.test(char);
+}
+
+function isChar(char) {
+  return /\w/.test(char);
+}
+
+function isDoubleQuote(char) {
+  return /\"/.test(char);
+}
+
+function isBracket(char) {
+  return /[\[\]\{\}]/.test(char);
+}
+
+function isKeyword(word) {
+  return /true|false|null/.test(word);
+}
