41d92d05a409f667a6eb748c94d44eff1d8be2da
Jason Barr <jason@jasonsbarr.com>
2021-04-28 08:46:00 -0500

    Add first lexer test

---
+++ json/tests/lexer.test.js
@@ -0,0 +1,13 @@
+const Lexer = require("../src/lexer");
+
+describe("Create a Lexer object", () => {
+  it("Should create a Lexer object with the appropriate properties", () => {
+    const lexer = new Lexer("abc");
+    const match = {
+      input: "abc",
+      pos: 0
+    };
+
+    expect(lexer).toMatchObject(match);
+  });
+})
