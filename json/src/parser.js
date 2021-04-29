7ae1e735201f44e1ec5861331e5aec2cc141f1ac
Jason Barr <jason@jasonsbarr.com>
2021-04-29 02:06:27 -0500

    Add helper functions for parser

---
+++ json/src/parser.js
@@ -0,0 +1,37 @@
+function createNode({ type, value, start, end }) {
+  return {
+    type,
+    value,
+    start,
+    end,
+  };
+}
+
+function isPunc(token) {
+  return token.type == "Punc";
+}
+
+function isString(token) {
+  return token.type == "String";
+}
+
+function isNumber(token) {
+  return token.type == "Number";
+}
+
+function isBoolean(token) {
+  return token.type == "Boolean";
+}
+
+function isNull(token) {
+  return token.type == "Null";
+}
+
+function getPunc(token) {
+  if (isPunc(token)) {
+    return token.value;
+  }
+  throw new Error(
+    `Expected punctuation token, got ${token.type} at ${token.start}:${token.end}`
+  );
+}
